369206972a68f8597ba8fd629c49a9fae7ab9fbd
Yuma <zheka218@bk.ru>
2023-10-02 00:53:03 +0300

    first commit

--- pythonProject/Square.py
+++ pythonProject/Square.py
@@ -1,20 +1,43 @@
 import math
 
-print("Введите коэффициенты для уравнения")
-print("ax^2 + bx + c = 0:")
-a = float(input("a = "))
-b = float(input("b = "))
-c = float(input("c = "))
 
-discr = b ** 2 - 4 * a * c
-print("Дискриминант D = %.2f" % discr)
+def square():
+    print("Введите коэффициенты для уравнения \nax^2 + bx + c = 0:")
 
-if discr > 0:
-    x1 = (-b + math.sqrt(discr)) / (2 * a)
-    x2 = (-b - math.sqrt(discr)) / (2 * a)
-    print("x1 = %.2f \nx2 = %.2f" % (x1, x2))
-elif discr == 0:
-    x = -b / (2 * a)
-    print("x = %.2f" % x)
-else:
-    print("Корней нет")
+
+    try:
+        a = float(input("a = "))
+    except ValueError:
+        print("Ошибка 1 \nНеверный формат введённых данных")
+        return
+
+    try:
+        b = float(input("b = "))
+    except ValueError:
+        print('Ошибка 1 \nНеверный формат введённых данных')
+        return
+
+    try:
+        c = float(input("c = "))
+    except ValueError:
+        print('Ошибка 1 \nНеверный формат введённых данных')
+        return
+
+    discr = b ** 2 - 4 * a * c
+    print("Дискриминант D = %.2f" % discr)
+
+    if discr > 0:
+        x1 = (-b + math.sqrt(discr)) / (2 * a)
+        x2 = (-b - math.sqrt(discr)) / (2 * a)
+        print("x1 = %.2f \nx2 = %.2f" % (x1, x2))
+        return x1, x2
+    elif discr == 0:
+        x = -b / (2 * a)
+        print("x = %.2f" % x)
+        return x
+    else:
+        print("Корней нет")
+        return None
+
+
+square()
