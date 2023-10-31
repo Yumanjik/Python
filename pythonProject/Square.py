81e3ee01303b2e849e80d8044e49ea01cd230cc6
Yuma <zheka218@bk.ru>
2023-11-01 00:27:46 +0300

    square 01.11.23

--- pythonProject/Square.py
+++ pythonProject/Square.py
@@ -1,43 +1,109 @@
 # Данная программа решает квадратные уравнения вида ax^2 + bx + c = 0 и выводит его корни
-import math
 
 
-def square():
-    print("Введите коэффициенты для уравнения \nax^2 + bx + c = 0:")
+import math                     #Библиотеки необходимые для работы программы
+from tkinter import *
+from tkinter import messagebox
 
-    try:
-        a = float(input("a = "))
-    except ValueError:
-        print("Ошибка 1 \nНеверный формат введённых данных")
-        return 'Ошибка 1'
+
+def square(a, b, c):            #Функция нахождения корней квадратного уравнения
 
     try:
-        b = float(input("b = "))
-    except ValueError:
-        print('Ошибка 1 \nНеверный формат введённых данных')
-        return 'Ошибка 1'
+
+        discr = b ** 2 - 4 * a * c  # Нахождение дискриминанта
+
+        if discr > 0:   # Нахождение корней квадратного уравнения, если дискриминант больше нуля
+
+            x1 = (-b + math.sqrt(discr)) / (2 * a)
+            x2 = (-b - math.sqrt(discr)) / (2 * a)
+
+            return "x1 = %.2f\nx2 = %.2f" % (x1, x2)
+
+        elif discr == 0:  # Нахождение корня квадратного уравнения, если дискриминант равен нулю
+
+            x = -b / (2 * a)
+            return "x = %.2f" % x
+
+        else:   # Корней квадратного уравнения нет, так как дискриминант меньше нуля
+
+            return "Корней нет"
+
+    except ZeroDivisionError:   #Отлов ошибки деления на 0
+
+        return 'Ошибка 2\nКоэффициент "а" не может быть нулём'
+
+
+def btnclick():
 
     try:
-        c = float(input("c = "))
+
+        a = float(ent_1.get())
+        b = float(ent_2.get())
+        c = float(ent_3.get())
+
+        anslbl["text"] = ''
+        anslbl["text"] = square(a, b, c)
+
     except ValueError:
-        print('Ошибка 1 \nНеверный формат введённых данных')
-        return 'Ошибка 1'
 
-    discr = b ** 2 - 4 * a * c  # Нахождение дискриминанта
-    print("Дискриминант D = %.2f" % discr)
+        anslbl["text"] = ''
+        anslbl["text"] = 'Ошибка 1 \nНеверный формат введённых данных'
 
-    if discr > 0:
-        x1 = (-b + math.sqrt(discr)) / (2 * a)  # Нахождение корней квадратного уравнения, если дискриминант больше нуля
-        x2 = (-b - math.sqrt(discr)) / (2 * a)
-        print("x1 = %.2f \nx2 = %.2f" % (x1, x2))
-        return x1, x2
-    elif discr == 0:  # Нахождение корня квадратного уравнения, если дискриминант равен нулю
-        x = -b / (2 * a)
-        print("x = %.2f" % x)
-        return x
-    else:
-        print("Корней нет")  # Корней квадратного уравнения нет, так как дискриминант меньше нуля
-        return None
+        return 'Ошибка 1 \nНеверный формат введённых данных'
+
+    return
 
 
-square()
+def on_close():             # Функция проверки желания закрытия окна
+
+    if messagebox.askokcancel('Выход', 'Действительно хотите закрыть окно?'):
+
+        mainwindow.destroy()
+
+
+# Создание и настройка интерфейса
+mainwindow = Tk()
+
+# Основное окно
+mainwindow.title('Калькулятор квадратных уравнений')
+mainwindow.geometry('400x90')
+mainwindow.resizable(width=False, height=False)
+mainwindow.protocol('WM_DELETE_WINDOW', on_close)
+
+# Главная надпись
+mainlbl = Label(mainwindow, text='Введите коэффициенты квадратного уравнения вида ax^2 + bx + c = 0 ')
+mainlbl.place(x=5, y=0)
+
+# Надписи
+lbl_1 = Label(mainwindow, text='x^2+')
+lbl_1.place(x=40, y=20)
+
+lbl_2 = Label(mainwindow, text='x+')
+lbl_2.place(x=105, y=20)
+
+lbl_3 = Label(mainwindow, text='= 0')
+lbl_3.place(x=160, y=20)
+
+# Окна ввода
+ent_1 = Entry(mainwindow, width=5)
+ent_1.place(x=5, y=22)
+ent_1.insert(0, '0')
+
+ent_2 = Entry(mainwindow, width=5)
+ent_2.place(x=70, y=22)
+ent_2.insert(0, '0')
+
+ent_3 = Entry(mainwindow, width=5)
+ent_3.place(x=125, y=22)
+ent_3.insert(0, '0')
+
+# Кнопка
+ansbtn = Button(mainwindow, text='Искать Ответ', command=lambda: btnclick())
+ansbtn.place(x=310, y=50)
+
+# Ответ
+anslbl = Label(mainwindow)
+anslbl.place(x=5, y=50)
+
+# Поддержание интерфейса
+mainwindow.mainloop()
